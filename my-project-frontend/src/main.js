e17ac607524844005c7dae93bd14fbfeae5493cb
scc <2020175482@qq.com>
2024-04-25 09:00:07 +0800

    前端目录大致页面

--- my-project-frontend/src/main.js
+++ my-project-frontend/src/main.js
@@ -3,7 +3,7 @@ import App from "@/App.vue";
 import router from "@/router/index.js";
 import axios from "axios";
 import {createPinia} from "pinia";
-
+import 'element-plus/theme-chalk/el-message.css';
 axios.defaults.baseURL = 'http://localhost:8080'
 const app = createApp(App)
 app.use(router)
